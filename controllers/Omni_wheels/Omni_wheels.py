8fa583ceae2b9cefe073774624d05f7502898a43
Nicher1 <nichlasost@gmail.com>
2022-05-06 22:52:52 +0200

    Motor functions completely

--- controllers/Omni_wheels/Omni_wheels.py
+++ controllers/Omni_wheels/Omni_wheels.py
@@ -1,53 +1,44 @@
 from controller import Robot
+import numpy as np
 import math
+# Robot is defined
 robot = Robot()
-# get the time step of the current world.
+# Get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
+v_len = np.linalg.norm # Length of vector
+pi = math.pi
+
 class driver:
-    def __init__(self, motor1, angle1, motor2, angle2, motor3, angle3):
-        self.motor_1 = motor1
+    def __init__(self, angle1, angle2, angle3):
+        self.motor_1 = robot.getDevice('wheel1')
+        self.motor_2 = robot.getDevice('wheel2')
+        self.motor_3 = robot.getDevice('wheel3')
         self.angle_1 = angle1
-        self.motor_2 = motor2
         self.angle_2 = angle2
-        self.motor_3 = motor3
         self.angle_3 = angle3
 
-    def driver(self, speed, angle):
-        print("tisse 2")
-        dir = self.polar_to_cartesian(angle, speed) # Asbjørn, speed er ordinært et 'distance' parameter
-        print("tisse 1")
+    def driver(self, distance, angle):
+        dir = self.polar_to_cartesian(angle, distance)
         m1 = self.polar_to_cartesian(self.angle_1, 1)
         m2 = self.polar_to_cartesian(self.angle_2, 1)
         m3 = self.polar_to_cartesian(self.angle_3, 1)
         v1 = self.vec_pos(m1, dir)
         v2 = self.vec_pos(m2, dir)
         v3 = self.vec_pos(m3, dir)
-        self.motor_1.setVelocity(vec_len(v1))
-        self.motor_2.setVelocity(vec_len(v2))
-        self.motor_3.setVelocity(vec_len(v3))
+        self.motor_1.setVelocity(v_len(v1))
+        self.motor_2.setVelocity(v_len(v2))
+        self.motor_3.setVelocity(v_len(v3))
 
     def polar_to_cartesian(self, angle, length):
-        return ([math.cos(angle) * length,math.sin(angle) * length])
-
-    def dot(self, a,b):
-        return a[0]*b[0]+a[1]*b[1]
-
-    def vec_scalar(self, a ,vet):
-        return [a*vet[0], a*vet[1]]
-
-    def vec_plus(self, a,b):
-        return [a[0]+b[0],a[1]+b[1]]
+        return np.array([math.cos(angle) * length, math.sin(angle) * length])
 
     def vec_projection(self, a,b):
-        print('floats: {}{}'.format(a,b))
-        return self.vec_scalar((self.dot(a,b))/(self.vec_len(b)),b)
+        # print('floats: {}{}'.format(a,b))
+        return (np.dot(a,b)/v_len(b)**2)*b
 
     def vec_pos(self, a, b):
-        return self.vec_plus(b, self.vec_scalar(-1, self.vec_projection(a, b)))
-
-    def vec_len(self, a):
-        return math.sqrt(a[0]**2+a[1]**2)
+        return -1*self.vec_projection(a, b)+b
 
     def print_var(self):
         print(self.motor_1)
@@ -58,14 +49,10 @@ class driver:
         print(self.angle_3)
 
 #insedires
-v = -10
-motor1 = robot.getDevice('wheel1')
-motor2 = robot.getDevice('wheel2')
-motor3 = robot.getDevice('wheel3')
+# v = -10
 ds = robot.getDevice('position_sensor_wheel1')
 ds.enable(timestep)
-
-run = driver(motor1 , 0, motor2 , 2*math.pi/3, motor3, 4*math.pi/3)
+run = driver(0, 2*pi/3, 4*pi/3)
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
@@ -73,14 +60,15 @@ run = driver(motor1 , 0, motor2 , 2*math.pi/3, motor3, 4*math.pi/3)
 
 while robot.step(timestep) != -1:
    ###################### - Big wheels - ###########################
-    motor1.setPosition(float('INF'))
-    motor2.setPosition(float('INF'))
-    motor3.setPosition(float('INF'))
+    run.motor_1.setPosition(0)
+    run.motor_2.setPosition(0)
+    run.motor_3.setPosition(0)
     #motor1.setVelocity(v)
     #motor2.setVelocity(-v)
     #motor3.setVelocity(0.0)
-    print("hej")
-    run.driver(1,math.pi)
+    #print("hi")
+    run.driver(10,0*pi)
+    run.print_var()
 
     ###################### - Small whells - #####################
     """ 10 is the max-speed of the small wheels"""
@@ -88,3 +76,18 @@ while robot.step(timestep) != -1:
     #motor1.setVelocity(10) #small wheels
     #motor2.setVelocity(10) #small wheels
     #motor3.setVelocity(10) #small wheels
+
+
+    # Deprecated functions written without use of numPy:
+
+    # def dot(self, a,b):
+    #     return a[0]*b[0]+a[1]*b[1]
+
+    # def vec_len(self, a):
+    #     return math.sqrt(a[0]**2+a[1]**2)
+
+    # def vec_plus(self, a,b):
+    #     return [a[0]+b[0],a[1]+b[1]]
+
+    # def vec_scalar(self, a ,vet):
+    #     return [a*vet[0], a*vet[1]]
---
+++ controllers/Targetbot/Targetbot.py
@@ -0,0 +1,42 @@
+from controller import Robot, DistanceSensor
+robot = Robot()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+#inseretes
+v = -10
+motor1 = robot.getDevice('wheel1')
+motor2 = robot.getDevice('wheel2')
+motor3 = robot.getDevice('wheel3')
+# dist1  = robot.getDevice('
+
+
+
+ds = robot.getDevice('position_sensor_wheel1')
+ds.enable(timestep)
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+
+
+while robot.step(timestep) != -1:
+   ###################### - Big wheels - ###########################
+    motor1.setPosition(float('INF'))
+    motor2.setPosition(float('INF'))
+    motor3.setPosition(float('INF'))
+    motor1.setVelocity(0.80*v)
+    motor2.setVelocity(-0.80*v)
+    motor3.setVelocity(0.30*v)
+
+
+
+
+
+
+    ###################### - Small whells - #####################
+    """ 10 is the max-speed of the small wheels"""
+    """ Velorcity of the big wheels until  """
+    #motor1.setVelocity(10) #small wheels
+    #motor2.setVelocity(10) #small wheels
+    #motor3.setVelocity(10) #small wheels
